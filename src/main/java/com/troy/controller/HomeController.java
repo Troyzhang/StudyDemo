13b0dfba5dada55a0336ed89e6779c844fa4ff4c
TroyZhang <yuecheung@live.com>
2016-09-02 17:37:12 +0800

    first commit

---
+++ src/main/java/com/troy/controller/HomeController.java
@@ -0,0 +1,29 @@
+package com.troy.controller;
+
+//import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by zhangyongyu on 16/9/2.
+ *
+ * @parse com.troy.controller
+ */
+// 注解标注此类为springmvc的controller，url映射为"/home"
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+    //添加一个日志器
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    //映射一个action
+    @RequestMapping("/index")
+    public String index() {
+        //输出日志文件
+        logger.info("the first jsp pages");
+        //返回一个index.jsp这个视图
+        return "index";
+    }
+}
