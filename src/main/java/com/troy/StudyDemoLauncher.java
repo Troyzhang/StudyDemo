0656d5b7acfd8cd780d0447ef954f66f321342e1
TroyZhang <yuecheung@live.com>
2016-09-13 16:16:34 +0800

    first commit

---
+++ src/main/java/com/troy/StudyDemoLauncher.java
@@ -0,0 +1,36 @@
+package com.troy;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.SessionManager;
+import org.eclipse.jetty.server.session.HashSessionManager;
+import org.eclipse.jetty.server.session.SessionHandler;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+public class StudyDemoLauncher {
+
+    public static void main(String[] args) throws Exception {
+
+        String path = Thread.currentThread().getContextClassLoader().getResource("").getFile();
+        path = path.replace("target/classes/", "");
+        path = path.replace("classes/", "");
+        Server server = new Server(8096);
+        String web = path + "src/main/webapp/";
+        WebAppContext context = new WebAppContext();
+        context.setDescriptor(web + "WEB-INF/web.xml");
+        context.setResourceBase(web);
+        context.setContextPath("/");
+        context.setParentLoaderPriority(true);
+        context.setMaxFormContentSize(600000);
+        //新增jetty的session保持会话时间
+        SessionHandler hand = new SessionHandler();
+        SessionManager manager = new HashSessionManager();
+        manager.setMaxInactiveInterval(86400);
+        hand.setSessionManager(manager);
+        context.setSessionHandler(hand);
+        server.setHandler(context);
+        server.start();
+        server.join();
+
+
+    }
+}
